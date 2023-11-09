f9bff994affe990f98ece5b15d39ecd8b30b3698
bittertwee42 <150373960+bittertwee42@users.noreply.github.com>
2023-11-09 18:01:13 +0100

    cambiato ma ancora da sistemare

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -23,17 +23,20 @@ int	ft_atoi(char *str)
 	i = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
 		i++;
-	if (str[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
+    while (str[i] == '+' || str[i] == '-')
+    {
+        if (str[i] == '-')
+        {
+            sign = -1;
+        }
+        i++;
+    }
 	while (str[i] >= 48 && str[i] <= 57)
 	{
 		res = res * 10 + (str[i] - '0');
 		i++;
 	}
-	if (res == -2147483648)
+	if (res == -2147483648) //modificare questo controllo
 	{
 		write(1, "-2147483648", 11);
 	}
